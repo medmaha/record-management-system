d01cffbea565161152b6735d6cc5650ca85193fc
medmaha <toure925@outlook.com>
2023-09-28 16:44:55 +0000

    intergrated superbase db x2

--- src/services/db/index.ts
+++ src/services/db/index.ts
@@ -1,16 +1,16 @@
 import { PG_DATABASE, PG_HOST, PG_PASSWORD, PG_PORT, PG_USER } from '$env/static/private';
 import { drizzle, type NodePgDatabase } from 'drizzle-orm/node-postgres';
-import { Client } from 'pg';
 import { migrate } from 'drizzle-orm/node-postgres/migrator';
+
+import pkg from 'pg';
+const { Client } = pkg;
 
 import * as schema from './schema';
 
 const PORT = Number(PG_PORT);
 
-let client: Client | undefined;
+let client: typeof Client | undefined;
 let DB: NodePgDatabase<typeof schema>;
-
-console.log('Booting Database System...');
 
 try {
 	const _client =
