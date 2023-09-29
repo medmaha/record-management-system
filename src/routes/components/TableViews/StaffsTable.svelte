6b8336c0125982105bd5fcd1ebce62615cc7b0fd
medmaha <toure925@outlook.com>
2023-09-29 15:12:01 +0000

    updared staffs page ui

---
+++ src/routes/components/TableViews/StaffsTable.svelte
@@ -0,0 +1,56 @@
+<!-- create a html table -->
+<script lang="ts">
+	import { fade } from 'svelte/transition';
+	export let staffs: any[];
+</script>
+
+<div class="p-x">
+	<table class="table-auto shadow bg-white table w-full">
+		<thead>
+			<th class="font-medium text-sm">No:</th>
+			<th> Name </th>
+			<th> Position </th>
+			<th> Department </th>
+			<th> Address </th>
+			<th> Telephone </th>
+		</thead>
+		<tbody>
+			{#each staffs as staff, i}
+				<tr in:fade>
+					<td class="font-medium text-sm">
+						{i + 1}
+					</td>
+					<td>
+						{staff.name}
+					</td>
+					<td>
+						{staff.position || 'N/A'}
+					</td>
+					<td>
+						{staff.department || 'N/A'}
+					</td>
+					<td>
+						{staff.address?.name ? staff.address.name : 'N/A'}
+					</td>
+					<td>
+						{staff.telephone || 'N/A'}
+					</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+</div>
+
+<style>
+	table thead {
+		background: rgb(226, 226, 226);
+	}
+	table th {
+		text-align: left;
+	}
+
+	table th,
+	table td {
+		padding: 10px 12px;
+	}
+</style>
--- src/routes/staffs/+page.svelte
+++ src/routes/staffs/+page.svelte
@@ -3,6 +3,8 @@
 	import type { PageData, ActionData } from './$types';
 	import StaffCard from '../components/StaffCard.svelte';
 	import Heading from '../components/Heading.svelte';
+	import ViewSwitcher from '../components/ViewSwitcher.svelte';
+	import StaffsTable from '../components/TableViews/StaffsTable.svelte';
 
 	export let data: PageData;
 	export let form: ActionData;
@@ -18,23 +20,30 @@
 	const submitForm: SubmitFunction = ({ action, formData }) => {
 		return async ({ update, result }) => {
 			await update({ reset: false });
-			console.log(action);
 
 			const query = formData.get('query') as string;
 			const model = formData.get('model') as string;
-
 			// white a code that change the current url address without reloading the browser
 
 			window.history.pushState('', '', `?search=${query}&model=${model}`);
 		};
 	};
 
-	if (form) console.log(form);
+	let view = 'grid' as 'grid' | 'table';
+	const toggleView = (_view: typeof view) => {
+		view = _view;
+	};
 </script>
 
 <Heading {model} user={data.user} {submitForm} hideModels={true} />
 
-{#if !cached.get(current_snapshot)?.length}
+<ViewSwitcher {view} {toggleView} />
+
+{#if view === 'table'}
+	<div class="p-4 pt-0">
+		<StaffsTable staffs={cached.get(current_snapshot)} />
+	</div>
+{:else if !cached.get(current_snapshot)?.length}
 	<div class="w-full h-[90dvh] flex items-center justify-center">
 		<div class="p-10 bg-black bg-opacity-20 text-center rounded-2xl">
 			<p class="text-center font-semibold pb-2 text-xl tracking-wide">Found no staff record</p>
@@ -49,7 +58,7 @@
 		</div>
 	</div>
 {:else}
-	<div class="p-4">
+	<div class="p-4 pt-0">
 		<div class="grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
 			{#each cached.get(current_snapshot) as staff}
 				<StaffCard {staff} />
