3190acf188aa22db6a875dfc1b42c6f4697613ab
medmaha <toure925@outlook.com>
2023-09-30 02:03:03 +0000

    ..x

--- src/services/db/index.ts
+++ src/services/db/index.ts
@@ -24,7 +24,7 @@ const boot = async () => {
 	// });
 	const DB = drizzle(dbClient, { schema });
 
-	await migrate(DB, { migrationsFolder: './drizzle' });
+	// await migrate(DB, { migrationsFolder: './drizzle' });
 
 	return { DB, dbClient };
 };
